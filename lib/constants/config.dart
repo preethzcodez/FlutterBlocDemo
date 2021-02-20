8976dfe9c0ba324ba3e30a22df456c7d7a5f3de2
Preeth Prathapan <preethzcodez.dll@gmail.com>
2021-02-20 19:25:34 +0530

    api update

--- lib/constants/config.dart
+++ lib/constants/config.dart
@@ -2,7 +2,7 @@ class Config {
   static final String baseUrl = "http://api.tvmaze.com";
 
   static const Map END_POINTS = {
-    'showsList': '/shows'
+    'showsList': '/shows?page=1'
   };
 
   static String endPoint(endPoint) {
